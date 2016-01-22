3d0790f849be4d678728480685c4baec676badf2
Brian Stovall <hoppenmaus@gmail.com>
2016-01-22 16:03:55 -0500

    got image placed and percentage display code running

--- melanzana.js
+++ melanzana.js
@@ -4,8 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
 	var getDt = calcDt();
 	var veggies = ['eggplant.png'];
 
+
 	goButton.addEventListener('click', () => {
-		drawPic(veggies[0]);	
+		alert(document.body.style['background-color']);
+
+		drawPic(veggies[0],50);	
 	});
 
   //function that takes the percentage of the picture that should
@@ -13,14 +16,32 @@ document.addEventListener('DOMContentLoaded', () => {
 	function drawPic (picAddr, percent) {
 		var frag = document.createDocumentFragment();
 		var pic = frag.appendChild(document.createElement('img'));
+		var mask = frag.appendChild(document.createElement('div'));
+
 		pic.src = 'assets/' + picAddr;
+
+		//scale height to 60% of screen and keep aspect ratio
 		var scale = heightPercent(60) / pic.height ;
 		console.log(pic.width + ', ' + pic.height);
-		pic.style.position = 'absolute';
 		pic.width *= scale;
 		pic.height *= scale;
+
+		//position on screen
+		pic.style.position = 'absolute';
 		pic.style.top = heightPercent(15).toString() + 'px';
 		pic.style.left = widthPercent(40).toString() + 'px';
+		pic.style['z-index'] = '-50';
+
+		//now drop a mask of the
+		mask.style['background-color'] = 'black';// document.body.style['background-color'];
+		mask.style.position = 'absolute';
+		mask.style['min-width'] = pic.width + 'px';
+		mask.style['min-height'] = (pic.height * percent / 100) + 'px';
+		mask.style.top = pic.style.top;
+		mask.style.left = pic.style.left;
+		mask.style['z-index'] = '50';
+
+		//render to document
 		document.body.appendChild(frag);
 	}
 
