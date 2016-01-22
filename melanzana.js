19f35fe478e81b82802dbd21014f330adee190eb
Brian Stovall <hoppenmaus@gmail.com>
2016-01-22 15:02:10 -0500

    working on scale/position/animate code

--- melanzana.js
+++ melanzana.js
@@ -1,15 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
+	var grab = document.getElementById.bind(document);
+	var goButton = grab('goButton');
 	var getDt = calcDt();
+	var veggies = ['eggplant.png'];
 
-});
+	goButton.addEventListener('click', () => {
+		drawPic(veggies[0]);	
+	});
 
-//returns a function that gives the time
-//in seconds since it was last run
-function calcDt() {
-	var lastTime = Date.now();
-	return () => {
-		result = (Date.now() - lastTime) / 1000;
-		lastTime = Date.now();
-		return result;
+  //function that takes the percentage of the picture that should
+	//currently show and renders it
+	function drawPic (picAddr, percent) {
+		var frag = document.createDocumentFragment();
+		var pic = frag.appendChild(document.createElement('img'));
+		pic.src = 'assets/' + picAddr;
+		var scale = heightPercent(60) / pic.height ;
+		console.log(pic.width + ', ' + pic.height);
+		pic.style.position = 'absolute';
+		pic.width *= scale;
+		pic.height *= scale;
+		pic.style.top = heightPercent(15).toString() + 'px';
+		pic.style.left = widthPercent(40).toString() + 'px';
+		document.body.appendChild(frag);
 	}
-}
+
+	//simple helper making percentage-based calculations
+	function pctToPx (dimension, percent) {
+		return dimension * percent / 100;
+	}
+
+	//easy handles from percent screen width/height ->  px
+	var widthPercent = pctToPx.bind(null, document.body.offsetWidth);
+	var heightPercent = pctToPx.bind(null, document.body.offsetHeight);
+
+	//returns a function that gives the time
+	//in seconds since it was last run
+	function calcDt() {
+		var lastTime = Date.now();
+		return () => {
+			result = (Date.now() - lastTime) / 1000;
+			lastTime = Date.now();
+			return result;
+		}
+	}
+});
