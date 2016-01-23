a67a46cef15d9a433f48cb7e61fbe0684e77b552
Brian Stovall <hoppenmaus@gmail.com>
2016-01-23 14:09:23 -0500

    added a tomato. also made drawing code simpler and set up different modes for work/relax. need to fix incomprehensible image scaling bug

--- melanzana.js
+++ melanzana.js
@@ -7,49 +7,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	//starts an animation of the timer picture
 	//to complete in 'time' argument minutes
-	function animatePic(duration, picAddr) {
+	//take a style variable that can be 'work' or 'rest' for the two different display styles
+	function animatePic(duration, picAddr, style) {
 		//make dt timer
-		var duration = duration * 60 + 1;
+		var duration = duration * 60;
 		var seconds = 0;
 		var timer = calcDt();
-		window.requestAnimationFrame(animate);
 
+		//draw the initial image and get a reference to the mask
+		var mask = drawPic(picAddr);
+
+		window.requestAnimationFrame(animate);
+		
+		//tracks timing, changes the mask height, and renders the timer
 		function animate() {
-			//console.log('animating');
 			seconds += timer();
 			//make the clock readout
 			var timeLeft = duration - seconds;
 			var minutesLeft = Math.floor(timeLeft/60);
-			var secondsLeft = Math.floor(timeLeft % 60);
+			var secondsLeft = Math.ceil(timeLeft % 60);
 			if (minutesLeft < 0) minutesLeft = 0;
 			if (secondsLeft < 0) secondsLeft = 0;
 			clockPanel.textContent = ('0' + minutesLeft).slice(-2) + ':' + ('0' + secondsLeft).slice(-2);
 
-			//animate the picture
-			drawPic(picAddr, seconds/duration * 100);
+			//change the mask height
+			mask.style['min-height'] = (style === 'work') ? 
+				mask.maxHeight - (seconds/duration * mask.maxHeight) + 'px' :
+				(seconds/duration * mask.maxHeight) + 'px'; 
 
 			//recurse while needed
 			if (seconds < duration) window.requestAnimationFrame(animate);
 		}
 	}
 
-  //function that takes the percentage of the picture that should
-	//currently show and renders it
-	function drawPic (picAddr, percent) {
-		//first clear out the old entries, if they exist
-		dump('mask');
-		dump('pic');
-
-		var frag = document.createDocumentFragment();
-		var pic = frag.appendChild(document.createElement('img'));
-		var mask = frag.appendChild(document.createElement('div'));
-
+	//function that draws the picture and a div mask completely obscuring it,
+	//then returns the mask's element
+	function drawPic (picAddr) {
+		var pic = document.body.appendChild(document.createElement('img'));
+		var mask = document.body.appendChild(document.createElement('div'));
+		mask.id = 'mask';
+		pic.id = 'pic';
 		pic.src = 'assets/' + picAddr;
 
-		//scale smaller of w/h to 60% of screen w/h and keep aspect ratio
-		var scale = (heightPercent(100) < widthPercent(100)) ? heightPercent(60) / pic.height :
-				widthPercent(60) / pic.width;
-		pic.id = 'pic';
+		//choose the smaller screen dimension and scale the matching
+		//pic dimension to 60%, then scale the other dimension accordingly to keep a/r
+		var scale = (heightPercent(100) < widthPercent(100)) ?
+			heightPercent(60) / pic.height :
+			widthPercent(60) / pic.width;
+		if (scale > 1) scale = 1/scale;
+
 		pic.width *= scale;
 		pic.height *= scale;
 
@@ -60,18 +66,18 @@ document.addEventListener('DOMContentLoaded', () => {
 		pic.style['z-index'] = '-50';
 
 		//now drop a mask of the background color over the image
-		//only leaving *percent* revealed
-		mask.id = 'mask';
 		mask.style['background-color'] = document.body.style['background-color'];
 		mask.style.position = 'absolute';
 		mask.style['min-width'] = pic.width + 'px';
-		mask.style['min-height'] = (pic.height * (100 - percent) / 100) + 'px';
+		mask.style['min-height'] = pic.height + 'px';
 		mask.style.top = pic.style.top;
 		mask.style.left = pic.style.left;
 		mask.style['z-index'] = '50';
+		//this is a reference to the final height of the full mask
+		mask.maxHeight = pic.height;
 
-		//render to document
-		document.body.appendChild(frag);
+		//return the mask element
+		return mask;
 	}
 	
 	//function for deleting elements by id
@@ -102,6 +108,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	//test
-	animatePic(1.25, 'watermelon.png');
+	animatePic(1, 'tomato.png', 'rest');
 });
 
